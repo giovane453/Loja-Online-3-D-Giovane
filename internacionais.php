0df253e65e5533ea5e798b4c8232617e6a222978
Giovane Alves <163547277+giovane453@users.noreply.github.com>
2025-06-26 10:41:51 -0300

    Add files via upload

---
+++ internacionais.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Retrô's - Camisas de Times Oficiais</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <header>
+        <div class="logo">Retrô's<span>Futbol</span></div>
+        <nav>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="nacionais.php">Times Nacionais</a></li>
+                <li><a href="internacionais.php">Times Internacionais</a></li>
+                <li><a href="contato.php">Contato</a></li>
+            </ul>
+        </nav>
